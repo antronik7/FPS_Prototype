0806016f55e5550024b07e09aa3502d4c3ae9d37
antronik7 <antoineberthiaumedutil@gmail.com>
2023-08-13 23:42:13 -0400

    ADS
    -Changed to the new Unity Input System.
    -Added the ability to ADS.
    -Added gun sway.
    -Added Target 3D model.

--- FPS_Prototype/Assets/Scripts/PlayerController.cs
+++ FPS_Prototype/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private int yawSpeed = 160;
+    private Transform gun;
     [SerializeField]
-    private int pitchSpeed = 120;
+    private GameObject crosshair;
+
+    [SerializeField]
+    private float hipFireYawSpeed = 160;
+    [SerializeField]
+    private int hipFirePitchSpeed = 120;
+    [SerializeField]
+    private int hipFireExtraYawSpeed = 220;
+    [SerializeField]
+    private int hipFireExtraPitchSpeed = 0;
+    [SerializeField]
+    private float hipFireExtraSpeedRampUpTime = 0.33f;
+    [SerializeField]
+    private float hipFireExtraSpeedRampUpDelay = 0f;
     [SerializeField]
     private float innerDeadZone = 0.08f;
     [SerializeField]
@@ -15,24 +29,95 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private AnimationCurve joystickCurve;
     [SerializeField]
-    private int extraYawSpeed = 220;
-    [SerializeField]
-    private int extraPitchSpeed = 0;
-    [SerializeField]
-    private float extraSpeedRampUpTime = 0.33f;
-    [SerializeField]
-    private float extraSpeedRampUpDelay = 0f;
-
-    [SerializeField]
     private int maxUpAngle = 90;
     [SerializeField]
     private int maxDownAngle = -90;
+    [SerializeField]
+    private float ADSyawSpeed = 160;
+    [SerializeField]
+    private int ADSpitchSpeed = 120;
+    [SerializeField]
+    private int ADSextraYawSpeed = 220;
+    [SerializeField]
+    private int ADSextraPitchSpeed = 0;
+    [SerializeField]
+    private float ADSextraSpeedRampUpTime = 0.33f;
+    [SerializeField]
+    private float ADSextraSpeedRampUpDelay = 0f;
+    [SerializeField]
+    private float ADSTriggerDeadZone = 0.25f;
+    [SerializeField]
+    private float ADSDuration = 0.25f;
+    [SerializeField]
+    private Vector3 hipFirePosition;
+    [SerializeField]
+    private Vector3 ADSPosition;
+    [SerializeField]
+    private int hipFireFOV = 70;
+    [SerializeField]
+    private int ADSFOV = 35;
+    [SerializeField]
+    private float maxSwayRotation = 5f;
+    [SerializeField]
+    private float maxSwayADSRotation = 0.5f;
+    [SerializeField]
+    private float maxSwayMagnitude = 0.25f;
 
+    private PlayerInputActions input = null;
+    private float leftTriggerValue = 0f;
+    private Vector2 rawJoystickInputs = Vector2.zero;
     private float camCurrentRotationX = 0f;
     private float camCurrentRotationY = 0f;
     private bool stickAtMaximum = false;
     private float extraSpeedRampUpTimer = 0f;
     private float extraSpeedDelayTimer = 0f;
+    private Vector3 gunDefaultRotation;
+
+    private void Awake()
+    {
+        input = new PlayerInputActions();
+        gunDefaultRotation = gun.localEulerAngles;
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Aim.performed += OnAimPerformed;
+        input.Player.Aim.canceled += OnAimCanceled;
+        input.Player.ADS.performed += OnADSPerformed;
+        input.Player.ADS.canceled += OnADSCanceled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Aim.performed -= OnAimPerformed;
+        input.Player.Aim.canceled -= OnAimCanceled;
+        input.Player.ADS.performed -= OnADSPerformed;
+        input.Player.ADS.canceled -= OnADSCanceled;
+    }
+
+    private void OnAimPerformed(InputAction.CallbackContext value)
+    {
+        rawJoystickInputs = value.ReadValue<Vector2>();
+    }
+
+    private void OnAimCanceled(InputAction.CallbackContext value)
+    {
+        rawJoystickInputs = Vector2.zero;
+    }
+
+    private void OnADSPerformed(InputAction.CallbackContext value)
+    {
+        leftTriggerValue = value.ReadValue<float>();
+    }
+
+    private void OnADSCanceled(InputAction.CallbackContext value)
+    {
+        leftTriggerValue = 0f;
+        extraSpeedDelayTimer = 0f;// Maybe should create a function for that...
+        extraSpeedRampUpTimer = 0f;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -43,20 +128,65 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AimDownSight();
         RotateCamera();
+    }
+
+    private void AimDownSight()
+    {
+        Vector3 velocity = Vector3.zero;
+        float FOVvelocity = 0;
+
+        if (leftTriggerValue < ADSTriggerDeadZone)
+        {
+            crosshair.SetActive(true);
+            gun.localPosition = Vector3.SmoothDamp(gun.localPosition, hipFirePosition, ref velocity, ADSDuration);
+            Camera.main.fieldOfView = Mathf.SmoothDamp(Camera.main.fieldOfView, hipFireFOV, ref FOVvelocity, ADSDuration);
+        }
+        else
+        {
+            crosshair.SetActive(false);
+            gun.localPosition = Vector3.SmoothDamp(gun.localPosition, ADSPosition, ref velocity, ADSDuration);
+            Camera.main.fieldOfView = Mathf.SmoothDamp(Camera.main.fieldOfView, ADSFOV, ref FOVvelocity, ADSDuration);
+        }
     }
 
     private void RotateCamera()
     {
-        float joystickHorixontalAxis = Input.GetAxis("CamHorizontal");
-        float joystickVerticalAxis = Input.GetAxis("CamVertical");
+        //float joystickHorixontalAxis = Input.GetAxis("CamHorizontal");
+        //float joystickVerticalAxis = Input.GetAxis("CamVertical");
 
-        Vector2 joystickInputs = new Vector2(joystickHorixontalAxis, joystickVerticalAxis);
+        //Vector2 joystickInputs = new Vector2(joystickHorixontalAxis, joystickVerticalAxis); 
+
+        Vector2 joystickInputs = rawJoystickInputs;
         joystickInputs = ApplyDeadZone(joystickInputs, innerDeadZone, outerDeadZone);
         joystickInputs = ApplyJoystickCurve(joystickInputs);
 
-        float currentYawSpeed = yawSpeed;
-        float currentPitchSpeed = pitchSpeed;
+        float currentYawSpeed;
+        float currentPitchSpeed;
+        int currentExtraYawSpeed;
+        int currentExtraPitchSpeed;
+        float currentExtraSpeedRampUpTime;
+        float currentExtraSpeedRampUpDelay;
+
+        if (leftTriggerValue < ADSTriggerDeadZone)
+        {
+            currentYawSpeed = hipFireYawSpeed;
+            currentPitchSpeed = hipFirePitchSpeed;
+            currentExtraYawSpeed = hipFireExtraYawSpeed;
+            currentExtraPitchSpeed = hipFireExtraPitchSpeed;
+            currentExtraSpeedRampUpTime = hipFireExtraSpeedRampUpTime;
+            currentExtraSpeedRampUpDelay = hipFireExtraSpeedRampUpDelay;
+        }
+        else
+        {
+            currentYawSpeed = ADSyawSpeed ;
+            currentPitchSpeed = ADSpitchSpeed;
+            currentExtraYawSpeed = ADSextraYawSpeed;
+            currentExtraPitchSpeed = ADSextraPitchSpeed;
+            currentExtraSpeedRampUpTime = ADSextraSpeedRampUpTime;
+            currentExtraSpeedRampUpDelay = ADSextraSpeedRampUpDelay;
+        }
 
         if (stickAtMaximum)
         {
@@ -70,10 +200,10 @@ public class PlayerController : MonoBehaviour
             {
                 extraSpeedDelayTimer += Time.deltaTime;
 
-                if (extraSpeedDelayTimer >= extraSpeedRampUpDelay)
+                if (extraSpeedDelayTimer >= currentExtraSpeedRampUpDelay)
                 {
                     if (extraSpeedRampUpTimer == 0f)
-                        extraSpeedRampUpTimer += extraSpeedDelayTimer - extraSpeedRampUpDelay;
+                        extraSpeedRampUpTimer += extraSpeedDelayTimer - currentExtraSpeedRampUpDelay;
                     else
                         extraSpeedRampUpTimer += Time.deltaTime;
                 }
@@ -84,21 +214,47 @@ public class PlayerController : MonoBehaviour
             stickAtMaximum = true;
         }
 
-        if(stickAtMaximum && extraSpeedDelayTimer >= extraSpeedRampUpDelay)
+        if(stickAtMaximum && extraSpeedDelayTimer >= currentExtraSpeedRampUpDelay)
         {
-            float rampUpPercent = extraSpeedRampUpTimer / extraSpeedRampUpTime;
+            float rampUpPercent = extraSpeedRampUpTimer / currentExtraSpeedRampUpTime;
             rampUpPercent = Mathf.Clamp01(rampUpPercent);
 
-            currentYawSpeed += extraYawSpeed * rampUpPercent;
-            currentPitchSpeed += extraPitchSpeed * rampUpPercent;
+            currentYawSpeed += currentExtraYawSpeed * rampUpPercent;
+            currentPitchSpeed += currentExtraPitchSpeed * rampUpPercent;
         }
 
+        if (leftTriggerValue < ADSTriggerDeadZone)
+        {
 
-        camCurrentRotationY += joystickInputs.y * currentYawSpeed * Time.deltaTime;
-        camCurrentRotationX += joystickInputs.x * currentPitchSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentYawSpeed *= ((float)ADSFOV / hipFireFOV);
+            currentPitchSpeed *= ((float)ADSFOV / hipFireFOV);
+        }
+
+        camCurrentRotationY += joystickInputs.x * currentYawSpeed * Time.deltaTime;
+        camCurrentRotationX += joystickInputs.y * currentPitchSpeed * Time.deltaTime;
         camCurrentRotationX = Mathf.Clamp(camCurrentRotationX, maxDownAngle, maxUpAngle);
 
         Camera.main.transform.rotation = Quaternion.Euler(camCurrentRotationX, camCurrentRotationY, 0);
+
+        SwayGun(joystickInputs);
+    }
+
+    private void SwayGun(Vector2 joystickInputs)
+    {
+        float swayrotation;
+
+        if (leftTriggerValue < ADSTriggerDeadZone)
+            swayrotation = maxSwayRotation;
+        else
+            swayrotation = maxSwayADSRotation;
+
+        float newAngleX = gunDefaultRotation.x + Mathf.Clamp(-swayrotation * (joystickInputs.y / maxSwayMagnitude), -swayrotation, swayrotation);
+        float newAngleY = gunDefaultRotation.y + Mathf.Clamp(swayrotation * (joystickInputs.x / maxSwayMagnitude), -swayrotation, swayrotation);
+
+        gun.localEulerAngles = new Vector3(newAngleX, newAngleY, gun.localEulerAngles.z);
     }
 
     private Vector2 ApplyDeadZone(Vector2 joystickInputs, float innerDeadZone = 0.19f, float outerDeadZone = 1f)
@@ -122,6 +278,5 @@ public class PlayerController : MonoBehaviour
         float convertedMagnitude = joystickCurve.Evaluate(inputsMagnitude);
 
         return joystickInputs.normalized * convertedMagnitude;
-
     }
 }
